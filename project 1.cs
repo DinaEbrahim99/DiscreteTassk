5f4a174acf853e14a3197a55c91ce24416a3bd75
DinaEbrahim99 <121172483+DinaEbrahim99@users.noreply.github.com>
2022-12-21 05:26:49 -0800

    Add files via upload

---
+++ project 1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the last number");
+            int last = int.Parse(Console.ReadLine());
+
+            for (int i = first; i <= last; i++)
+            {
+                int flag = 1;
+
+                for (int j = 2; j < i; ++j)
+                {
+                    if (i % j == 0)
+                        flag = 0;
+                    if (flag == 1)
+                        Console.WriteLine(i);
+                }
+
+
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ project 2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, sum;
+            Console.WriteLine("enter the first number");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the last number");
+            int last = int.Parse(Console.ReadLine());
+            for (i = first; i<= last; i++)
+            {
+                  sum = 0;
+                for (j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        sum += j;
+                }
+                if (sum == i)
+                    Console.WriteLine(i);
+
+            }
+            Console.ReadKey();
+            
+            
+               
+                   
+            
+            
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
